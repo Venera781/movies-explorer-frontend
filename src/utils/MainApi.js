8bcc4e5579d4ff300b634e33874e2d1ed0a671b2
Venera <mv781781@yandex.ru>
2024-03-01 11:35:01 +0300

    Обновить urls

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -98,7 +98,7 @@ class MainApi extends Api {
 }
 
 const optionsApi = {
-  baseUrl: 'http://127.0.0.1:4000',
+  baseUrl: 'https://api.movie-m781.nomoredomainsmonster.ru',
   headers: {
     'Content-Type': 'application/json',
   },
